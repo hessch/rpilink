73d2934cd2ad0efdc82cd75010b030082f899a7d
Hessel Schut <hessel@isquared.nl>
2014-06-03 23:15:37 +0200

    allow for multiple links in c011 class

--- Inmos.py
+++ Inmos.py
@@ -11,6 +11,19 @@ CONTROL_I2C_ADDR = 0x21
 i2c = smbus.SMBus(0)
 
 class c011:
+	def __init__(self, dbus_addr=None, ctrl_addr=None):
+		"""create Inmos Link, optionally specifying i2c addresses"""
+		
+		if dbus_addr is None:
+			self.dbus_addr = DATABUS_I2C_ADDR
+		else:
+			self.dbus_addr = dbus_addr
+		
+		if ctrl_addr is None:
+			self.ctrl_addr = CONTROL_I2C_ADDR
+		else:
+			self.ctrl_addr = ctrl_addr
+		
 	# ctrl maps c011 control signals to PCF8574 bits
 	ctrl = [
 		'RS0', 'RS1', 'RnotW'
@@ -20,7 +33,7 @@ class c011:
 		"""Reset the C011 link adapter"""
 	
 		# make databus high for input
-		i2c.write_byte(DATABUS_I2C_ADDR, 0xff)
+		i2c.write_byte(self.dbus_addr, 0xff)
 	
 	def write_byte(data):
 		"""write data to the link"""
