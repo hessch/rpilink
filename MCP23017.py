16076889f940213b102666d72b27c97424cd4a6f
Hessel Schut <hessel@isquared.nl>
2014-11-07 09:09:45 +0100

    Added GPL license.

--- MCP23017.py
+++ MCP23017.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python
 
 # MCP23017 i2c gpio controller module
-# Hessel Schut, hessel@isquared.nl, 2014-10-12
+# Copyright (C) 2014 Hessel Schut, hessel@isquared.nl
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 
 import smbus
 
